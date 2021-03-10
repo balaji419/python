c2d5bfe90071c85c6574bbd4583b1769b69e9a68
balaji419 <33886990+balaji419@users.noreply.github.com>
2021-03-10 16:34:34 +0530

    Create get_creds.py

---
+++ get_creds.py
@@ -0,0 +1,14 @@
+import boto3,ast,subprocess
+
+from subprocess import PIPE
+def main():
+        process=subprocess.Popen(["aws","sts","assume-role","--role-arn","arn:aws:iam::712733279313:role/ec2-role","--role-session-name","get-creds"],stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        stdout=stdout.decode()
+        stdout=ast.literal_eval(stdout)
+        secret_key=stdout['Credentials'].get('SecretAccessKey')
+        session_token=stdout['Credentials'].get('SessionToken')
+        access_keyid=stdout['Credentials'].get('AccessKeyId')
+        return secret_key,session_token,access_keyid
+a,b,c=main()
+print(a,b,c)
